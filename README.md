6b9ee4b416080863b6bb3ebf97a4f72d2bf93bf8
PanYan <panyan5617998@163.com>
2013-05-08 18:25:46 +0800

    init reposite

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-online-tutoring-system
+﻿online-tutoring-system
 ======================
 专业综合项目
 base on struts2 and flash
+change
---
+++ src/com/test/action/LoginAction.java
@@ -0,0 +1,36 @@
+package com.test.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public String username;
+	public String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	public String execute() throws Exception
+	{
+		if("panyan".equals(username)&&"1111".equals(password))
+			return SUCCESS;
+		return ERROR;
+	}
+	
+}
+
